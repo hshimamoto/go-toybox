ab2184c10cd97ce03b056e10c3e2d8a9c0288fdf
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-07-26 18:45:45 +0900

    httpcat

---
+++ httpcat/main.go
@@ -0,0 +1,34 @@
+// simple-proxy
+// MIT License Copyright(c) 2018 Hiroshi Shimamoto
+// vim:set sw=4 sts=4:
+//
+// ./httpcat URL
+//
+
+package main
+
+import (
+    "net/http"
+    "os"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+	return
+    }
+    url := os.Args[1]
+    resp, err := http.Get(url)
+    if err != nil {
+	return
+    }
+    body := resp.Body
+    defer body.Close()
+    for {
+	buf := make([]byte, 8192)
+	n, err := body.Read(buf)
+	if err != nil {
+	    break
+	}
+	os.Stdout.Write(buf[:n])
+    }
+}
