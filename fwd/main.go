e8b9ae42fc13d5e93cd9632152eadef148c5f437
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2019-11-22 22:05:32 +0000

    fwd: add rate limit

--- fwd/main.go
+++ fwd/main.go
@@ -9,20 +9,62 @@ import (
     "log"
     "net"
     "os"
+    "strconv"
 
     "github.com/hshimamoto/go-session"
     "github.com/hshimamoto/go-iorelay"
+    "github.com/mxk/go-flowrate/flowrate"
 )
 
 func show_usage() {
-    fmt.Println("Version: fwd v1.0.0 in go-toyboxv")
-    fmt.Println("Usage: fwd <listen> <dest>")
+    fmt.Println("Version: fwd v2.0.0 in go-toyboxv")
+    fmt.Println(
+`Usage: fwd <listen> <dest> [KiB/s or MiB/s]
+ex)
+	fwd :8080 www.example.com:80
+	fwd :8080 www.example.com:80 100K
+	fwd :8080 www.example.com:80 10M`)
+}
+
+type FlowrateIO struct {
+    r *flowrate.Reader
+    w *flowrate.Writer
+}
+
+func (f *FlowrateIO)Read(p []byte) (int, error) {
+    return f.r.Read(p)
+}
+
+func (f *FlowrateIO)Write(p []byte) (int, error) {
+    return f.w.Write(p)
 }
 
 func main() {
     if len(os.Args) < 3 {
 	show_usage()
 	return
+    }
+    var mb int64 = 0
+    if len(os.Args) > 3 {
+	lim := os.Args[3]
+	unit := 0
+	switch lim[len(lim) - 1] {
+	case 'k': unit = 1024
+	case 'K': unit = 1024
+	case 'm': unit = 1024 * 1024
+	case 'M': unit = 1024 * 1024
+	}
+	m, err := strconv.Atoi(lim[:len(lim) - 1])
+	if err != nil {
+	    log.Printf("Atoi %v", err)
+	    m = 0
+	}
+	mb = int64(m * unit)
+	if mb == 0 {
+	    log.Printf("bad option: %s\n", lim)
+	    return
+	}
+	log.Printf("Set Ratelmit %d bytes/s", mb)
     }
     s, err := session.NewServer(os.Args[1], func(conn net.Conn) {
 	defer conn.Close()
@@ -32,7 +74,19 @@ func main() {
 	    return
 	}
 	defer fconn.Close()
-	iorelay.Relay(conn, fconn)
+	if mb > 0 {
+	    f1 := &FlowrateIO{
+		r: flowrate.NewReader(conn, mb),
+		w: flowrate.NewWriter(conn, mb),
+	    }
+	    f2 := &FlowrateIO{
+		r: flowrate.NewReader(fconn, mb),
+		w: flowrate.NewWriter(fconn, mb),
+	    }
+	    iorelay.Relay(f1, f2)
+	} else {
+	    iorelay.Relay(conn, fconn)
+	}
     })
     if err != nil {
 	log.Printf("session.NewServer: %v\n", err)
